823ef5bc397c6cceab29a912bc5a0bedf36f6b8b
lam218 <lucyamurray@me.com>
2020-07-13 18:23:42 +0100

    fix json issue

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('db.js');
+const data = require('./db.js')();
+const router = jsonServer.router(data);
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
-
+//console.log(require('./db.js')());
 server.use(middlewares);
 server.use(router);
 
